2b140ed134908049a4063e4faef5bdaa26cc9524
akane0915 <asialkane@gmail.com>
2017-07-15 17:49:53 -0700

    add unit testing for Product model including price numericality

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,6 @@
 class Product < ApplicationRecord
   has_many :order_items
+
+  validates :name, :description, :price, presence: true
+  validates :price, format: { with: /\A\d+(?:\.\d{0,2})?\z/ }, numericality: {greater_than: 0}
 end
