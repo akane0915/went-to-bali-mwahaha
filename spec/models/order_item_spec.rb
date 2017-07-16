85ca8eb34f5800c4d2c04d4efc8706bd14288203
akane0915 <asialkane@gmail.com>
2017-07-15 18:20:28 -0700

    add unit testing for Order Items model

---
+++ spec/models/order_item_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+describe OrderItem do
+  it { should belong_to :product }
+  it { should belong_to :order }
+
+  it { should validate_presence_of :quantity }
+  it { should validate_presence_of :product_id }
+  it { should validate_presence_of :order_id }
+end
